d53900687284cbdeee9c661542e94a9c8fdb7dae
zhisheng <zhishengtrieu@gmail.com>
2025-02-12 14:45:59 -0500

    feat: add more provider for auth

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,6 +2,10 @@ import NextAuth from 'next-auth';
 //import Credentials from 'next-auth/providers/credentials';
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import LinkedIn from "next-auth/providers/linkedin"
+import Facebook from 'next-auth/providers/facebook';
+import Twitter from 'next-auth/providers/twitter';
+import Apple from 'next-auth/providers/apple';
 
 export const {
   handlers: { GET, POST },
@@ -20,5 +24,17 @@ export const {
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
+    // Fournisseur LinkedIn
+    LinkedIn({
+      clientId: process.env.LINKEDIN_CLIENT_ID!,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET!,
+    }),
+    // Fournisseur Facebook
+    Facebook({
+      clientId: process.env.AUTH_FACEBOOK_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+    Twitter,
+    Apple,
   ],
 });
