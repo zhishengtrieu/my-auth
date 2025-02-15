d23da2f96709b8146873d6556cf288c20002249b
zhisheng <zhishengtrieu@gmail.com>
2025-02-15 12:38:59 -0500

    feat: add register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,10 +1,10 @@
 import { AuthForm } from "@/components/AuthForm"
 
-export default function LoginPage() {
+export default function RegisterPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <AuthForm mode="login" />
+        <AuthForm mode="register"/>
       </div>
     </div>
   )
