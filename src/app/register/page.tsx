5dc7567006023e56f2a2f46902acfe6ae3f8e665
zhisheng <zhishengtrieu@gmail.com>
2025-02-17 09:53:18 -0500

    refactor: enhance app readability

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,7 +4,7 @@ export default function RegisterPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <AuthForm mode="register"/>
+        <AuthForm mode="register" />
       </div>
     </div>
   )
